426440644bd0d212e4cd8b627c422cf3562880ce
tejvuligonda <tejvuligonda@gmail.com>
2016-04-16 10:44:20 -0700

    Added a UILabel for the display of the calculator

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,8 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController { // UIViewController is the parent class
+    
+    @IBOutlet weak var display: UILabel!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
