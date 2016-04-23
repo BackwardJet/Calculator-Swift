7ff72cd6cf482a500066349c525966bdc1b9545a
tejvuligonda <tejvuligonda@gmail.com>
2016-04-23 10:24:45 -0700

    Overloaded performOperation to support square root functionality

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -36,14 +36,21 @@ class ViewController: UIViewController { // UIViewController is the parent class
         case "-": performOperation {$1 - $0}
         case "✕": performOperation {$0 * $1}
         case "÷": performOperation {$1 / $0}
-        
+        case "√": performOperation {sqrt($0)}
         default: break
         }
     }
     
-    func performOperation(operation: (Double,Double) -> Double) {
+    private func performOperation(operation: (Double,Double) -> Double) {
         if operandStack.count >= 2 {
             displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    private func performOperation(operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
             enter()
         }
     }
